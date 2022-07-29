68ff03e242ef22584ae2bae2ba58945ae949c76a
Adrian Gomez <radegor@gmail.com>
2022-07-29 16:27:26 -0700

    feat: add merge two arrays function

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # js-adv-exercises
+
 JavaScript Advanced exercises and algorithms
+
+## Content
+
+### Algorithms
+
+- Merge two sorted arrays where one has enough space at the end for the other.
+
+## Author
+
+Adrian Gomez
+
+## License
+
+[MIT](LICENSE)
---
+++ src/algorithms/mergeTwoArrays.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+// Merge two sorted arrays where one has enough space at the end for the other
+function mergeTwoArrays(largeArray: number[], smallArray: number[]) {
+  let s = smallArray.length - 1;
+  let l1 = largeArray.length - 1;
+  let l0 = l1 - s - 1;
+  let insertFromSmall = false;
+
+  while (s >= 0) {
+    insertFromSmall = l0 < 0 || smallArray[s]! > largeArray[l0]!;
+    largeArray[l1--] = insertFromSmall ? largeArray[l0--]! : smallArray[s--]!;
+  }
+}
+
+export default mergeTwoArrays;
